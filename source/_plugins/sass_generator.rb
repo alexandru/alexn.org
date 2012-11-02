6135be94d57dc1a0deb48b0668894b1418dfff0b
Alexandru Nedelcu <noreply@alexn.org>
2012-11-02 11:38:44 +0200

    Fix

--- source/_plugins/sass_generator.rb
+++ source/_plugins/sass_generator.rb
@@ -63,8 +63,9 @@ module Jekyll
     def render(context)
       return '/assets/all.css' unless context['site']['build_type'] == 'production' and context['site']['css_hash'] == true
       path = File.join(File.dirname(__FILE__), '..', '..', 'build', 'assets', 'all.css')
-      css = File.exists?(path) ? File.read(path) : Time.now.strftime("%Y%m%d")
-      Utils.wrap_assets_link("/assets/all-" + Digest::MD5.hexdigest(css)[0,9] + ".css", context['site'])
+      return Utils.wrap_assets_link("/assets/all.css?dt=" + Time.now.strftime("%Y%m"), context['site'])
+      #css = File.exists?(path) ? File.read(path) : Time.now.strftime("%Y%m%d")
+      #Utils.wrap_assets_link("/assets/all-" + Digest::MD5.hexdigest(css)[0,9] + ".css", context['site'])
     end
   end
 end
