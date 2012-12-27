5e17c4ec2964788d7245608d109a2896e1da8e34
Alexandru Nedelcu <me@alexn.org>
2012-12-27 10:16:35 +0200

    Fix

--- source/_plugins/sass_generator.rb
+++ source/_plugins/sass_generator.rb
@@ -39,7 +39,7 @@ module Jekyll
         all_content << content
       end      
 
-      all_css = dest.join('assets', "all.css")
+      all_css = dest.join('assets', "all-v2.css")
       all_css.dirname.mkpath unless all_css.dirname.exist?
       File.open(all_css, 'w') do |fh|
         fh.write(all_content)
@@ -51,7 +51,7 @@ module Jekyll
         fh.write(all_content)
       end
 
-      site.static_files << Jekyll::AllCSSFile.new(site, site.dest, 'assets', "all.css")
+      site.static_files << Jekyll::AllCSSFile.new(site, site.dest, 'assets', "all-v2.css")
       site.static_files << Jekyll::AllCSSFile.new(site, site.dest, 'assets', digest_name)
     end    
   end
@@ -61,10 +61,11 @@ module Jekyll
     end
 
     def render(context)
-      return '/assets/all.css' unless context['site']['build_type'] == 'production' and context['site']['css_hash'] == true
-      path = File.join(File.dirname(__FILE__), '..', '..', 'build', 'assets', 'all.css')
-      css = File.exists?(path) ? File.read(path) : Time.now.strftime("%Y%m%d")
-      Utils.wrap_assets_link("/assets/all-" + Digest::MD5.hexdigest(css)[0,9] + ".css", context['site'])
+      return '/assets/all-v2.css' # unless context['site']['build_type'] == 'production' and context['site']['css_hash'] == true
+      
+      #path = File.join(File.dirname(__FILE__), '..', '..', 'build', 'assets', 'all-v2.css')
+      #css = File.exists?(path) ? File.read(path) : Time.now.strftime("%Y%m%d")
+      #Utils.wrap_assets_link("/assets/all-" + Digest::MD5.hexdigest(css)[0,9] + ".css", context['site'])
     end
   end
 end
