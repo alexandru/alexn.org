a88212eb32205048a086fb443b3da970eae42c36
Alexandru Nedelcu <noreply@alexn.org>
2013-01-11 09:50:38 +0200

    Fixed fonts

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem "middleman-blog"
 gem "middleman-smusher"
 gem "middleman-syntax"
 gem "middleman-sync"
+gem "middleman-minify-html"
 
 gem "builder", "~> 3.0.0"
 
--- config.rb
+++ config.rb
@@ -100,11 +100,9 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
+  activate :minify_html
 
   activate :asset_hash
 
