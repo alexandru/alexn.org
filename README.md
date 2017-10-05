460ead9adfdb961427ef8b44b609f23b01d754aa
Alexandru Nedelcu <noreply@alexn.org>
2017-10-05 18:07:07 +0300

    Travis build

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 The source-code of my personal blog: [alexn.org](https://alexn.org).
 
+[![Travis](https://img.shields.io/travis/alexandru/alexn.org.svg)](https://travis-ci.org/alexandru/alexn.org)
---
+++ misc/build-then-sync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`/..
+
+bundle exec middleman build
+mkdir -p $HOME/.ssh
+cp misc/travis_rsa $HOME/.ssh
+chmod -R go-rwx $HOME/.ssh
+
+rsync --delete-excluded -Pacv \
+  -e "ssh -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
+  build/ alex@alexn.org:/var/www/alexn.org/
