0bbfc41e870520693479b30fb33441606f480aa0
Alexandru Nedelcu <noreply@alexn.org>
2017-12-17 15:58:21 +0200

    Fix build

--- scripts/build-then-sync.sh
+++ scripts/build-then-sync.sh
@@ -8,5 +8,5 @@ cp scripts/travis_rsa $HOME/.ssh
 chmod -R go-rwx $HOME/.ssh
 
 rsync --delete-excluded -Pacv \
-  -e "ssh -e 223 -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
+  -e "ssh -p 223 -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
   build/ alex@alexn.org:/var/www/alexn.org/
