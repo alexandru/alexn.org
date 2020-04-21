44589a2a985a09e84d683cbd8cfd0bb1bd379c1c
Alexandru Nedelcu <noreply@alexn.org>
2020-04-21 21:17:46 +0300

    Fix build

--- _plugins/html_filters.rb
+++ _plugins/html_filters.rb
@@ -75,6 +75,12 @@ module Jekyll
       body ? doc.at_css("body").inner_html : ""
     end
   end
+
+  module MyFileFilters
+    def md5file(path)
+      puts site.static_files[path]
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::MyDateFilter)
