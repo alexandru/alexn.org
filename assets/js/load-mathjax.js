d20760043d0fc6b5ee1581edf24b2298eacce9d5
Alexandru Nedelcu <noreply@alexn.org>
2022-03-31 09:04:28 +0000

    docs: update gh-pages

---
+++ assets/js/load-mathjax.js
@@ -0,0 +1,47 @@
+(function () {
+  function triggerOnce(document, f) {
+    var wasLoaded = false;
+    return function () {
+      var shouldLoad = !wasLoaded && (
+        typeof document.readyState === "undefined" ||
+        document.readyState === "complete"
+      );
+      if (shouldLoad) {
+        wasLoaded = true;
+        f();
+      }
+    };
+  }
+
+  function loadScript() {
+    console.log("Loading MathJax")
+
+    function scriptCData(x) {
+      if (x.startsWith('% <![CDATA[') && x.endsWith('%]]>')) {
+        return x.substring(11,x.length-4);
+      }
+      return x;
+    }
+
+    document.querySelectorAll("script[type='math/tex']").forEach(function (el) {
+      el.outerHTML = "<div class='formula-code'><script type='math/text'>" + scriptCData(el.textContent) + "</script></div>";
+    });
+
+    document.querySelectorAll("script[type='math/tex; mode=display']").forEach(function(el){
+      el.outerHTML = "<div class='formula-code'><script type='math/tex; mode=display'>" + scriptCData(el.textContent) + "</script></div>";
+    });
+
+    var script = document.createElement('script');
+    script.src = "/assets/js-managed/mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+    script.async = true;
+    document.head.appendChild(script);
+  }
+
+  if (document.readyState === "complete") {
+    loadScript();
+  } else {
+    var loader = triggerOnce(document, loadScript);
+    document.addEventListener('DOMContentLoaded', loader, false);
+    document.onreadystatechange = loader;
+  }
+})();
---
+++ assets/js/scripts.js
@@ -0,0 +1,79 @@
+(function () {
+	function actOnDarkReaderExtension() {
+		var isDarkReaderEnabled =
+			"querySelector" in document &&
+			!!document.querySelector("meta[name=darkreader]");
+
+		if (isDarkReaderEnabled) {
+			$("html").addClass("dark-reader");
+
+			var showWarning =
+				"matchMedia" in window && 
+				window.matchMedia('(prefers-color-scheme: dark)').matches &&
+				!document.cookie.match(/accept_dark_reader/);
+
+			if (showWarning) {
+				document.cookie = "accept_dark_reader=1;path=/;max-age=604800";
+				alert(
+					"You have the Dark Reader extension enabled.\n\n" + 
+					"This website already supports a dark theme 🚀✨\n\n" + 
+					"Please disable Dark Reader for this website,\nas it interferes with its design 🙏"
+				);
+			}
+		}
+	}
+
+	/**
+	 * Adds the "dropcap" class automatically.
+	 */
+	function initDropCapParagraphs() {
+		var elem = $("p.intro.withcap:first").contents().filter(function () { return this.nodeType == 3 }).first();
+		var text = elem.text().replace(/^[\s\r\n]+/, "");
+		var first = text.slice(0, 1);
+
+		if (!elem.length) return;
+
+		elem[0].nodeValue = text.slice(first.length);
+		elem.before('<span class="dropcap">' + first + '</span>');
+
+		// DropCap.js
+		var dropcaps = document.querySelectorAll(".dropcap");
+		window.Dropcap.layout(dropcaps, 2);		
+	}
+
+	function initYouTubeWidget() {
+		$("figure.video img.play-thumb").one("load", function () {
+			var playImg = document.createElement("img");
+			playImg.src = "/assets/media/youtube.png";
+			playImg.alt = "";
+			playImg.className = "play-logo";
+			$(playImg).appendTo($(this).parent());
+		}).each(function () {
+			// https://stackoverflow.com/questions/3877027/jquery-callback-on-image-load-even-when-the-image-is-cached
+			if (this.complete) {
+				$(this).trigger("load");
+			}
+		});
+	}
+
+	function onDocumentReady() {
+		initDropCapParagraphs();
+		// Activate responsive navigation
+		responsiveNav(".nav-collapse");
+		// Activate YouTube Widgets
+		initYouTubeWidget();
+
+		// Round Reading Time
+		$(".time").text(function (_index, value) {
+			return Math.round(parseFloat(value));
+		});
+
+		actOnDarkReaderExtension();
+	}
+
+	if (document.readyState === "complete") {
+		onDocumentReady();
+	} else {
+		window.onload = onDocumentReady;
+	}
+})();
