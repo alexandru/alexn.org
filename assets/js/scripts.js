4e2b4745456e186db2a325026bafa80ec489f25a
Alexandru Nedelcu <noreply@alexn.org>
2023-05-25 16:15:23 +0300

    Optimize syntax highlighting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,9 +26,6 @@ layout: null
 		// Activate responsive navigation
 		responsiveNav(".nav-collapse");
 
-		// Activate syntax highlighting
-		hljs.highlightAll();
-
 		// Round Reading Time
 		$(".time").text(function (_index, value) {
 			return Math.round(parseFloat(value));
