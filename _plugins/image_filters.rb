b068bfc87c15b8eeddcca59f9873cdfeb87b825c
Alexandru Nedelcu <noreply@alexn.org>
2023-05-27 23:55:38 +0300

    Image decoding lazy

--- _plugins/image_filters.rb
+++ _plugins/image_filters.rb
@@ -3,7 +3,7 @@ require "fastimage"
 module MyImages
   @@site = Jekyll.configuration({})
   @@image_cache = {}
-  
+
   def self.size_of(path, log=false)
     local_path = "." + path.gsub(/^#{Regexp.quote(@@site['baseurl'])}|[?][^$]+$/, "")
     local_path = local_path.sub("/wiki/assets/", "/_wiki/assets/")
@@ -13,7 +13,7 @@ module MyImages
       end
       @@image_cache[local_path] = FastImage.size(local_path)
       puts "MyImages::size_of — Not cached: #{local_path}" if log
-    else 
+    else
       puts "MyImages::size_of — Cached: #{local_path}" if log
     end
     @@image_cache[local_path]
@@ -21,25 +21,26 @@ module MyImages
 end
 
 module Jekyll
-  module MyImageFilters 
+  module MyImageFilters
     def fix_images(html)
-      html.gsub(/<img([^>]+)src=(['"](\/[^'"]+)['"])([^>]+)>/) do |img| 
+      html.gsub(/<img([^>]+)src=(['"](\/[^'"]+)['"])([^>]+)>/) do |img|
         if img.include?("width=") || img.include?("height=")
           img
-        else 
+        else
           prefix = Regexp.last_match[1]
           img0 = Regexp.last_match[2]
           path = Regexp.last_match[3]
           suffix = Regexp.last_match[4]
-          
+          decoding = !img.include?("decoding=") ? " decoding=\"async\"" : ""
+
           size = MyImages.size_of(path)
           width = size[0]
           height = size[1]
-          "<img#{prefix}src=#{img0} width=\"#{width}\" height=\"#{height}\"#{suffix}>"
-        end 
+          "<img#{prefix}src=#{img0} width=\"#{width}\" height=\"#{height}\"#{decoding}#{suffix}>"
+        end
       end
     end
   end
 end
-  
+
 Liquid::Template.register_filter(Jekyll::MyImageFilters)
