3c3d32feaabd3ba04ebe1127cd46da70be7c4ed0
Alexandru Nedelcu <noreply@alexn.org>
2025-02-15 19:39:06 +0000

    docs: update gh-pages

--- assets/js/load-mathjax.js
+++ assets/js/load-mathjax.js
@@ -14,7 +14,7 @@
   }
 
   function loadScript() {
-    console.log("Loading MathJax")
+    console.log("Loading MathJax");
 
     function scriptCData(x) {
       if (x.startsWith('% <![CDATA[') && x.endsWith('%]]>')) {
@@ -23,16 +23,38 @@
       return x;
     }
 
+    // Convert existing math elements to new format
     document.querySelectorAll("script[type='math/tex']").forEach(function (el) {
-      el.outerHTML = "<div class='formula-code'><script type='math/text'>" + scriptCData(el.textContent) + "</script></div>";
+      el.outerHTML = "<div class='formula-code'><span class='math inline'>" + scriptCData(el.textContent) + "</span></div>";
     });
 
     document.querySelectorAll("script[type='math/tex; mode=display']").forEach(function(el){
-      el.outerHTML = "<div class='formula-code'><script type='math/tex; mode=display'>" + scriptCData(el.textContent) + "</script></div>";
+      el.outerHTML = "<div class='formula-code'><span class='math display'>" + scriptCData(el.textContent) + "</span></div>";
     });
 
+    // Load MathJax 3
+    window.MathJax = {
+      tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        packages: ['base', 'ams', 'noerrors', 'noundefined']
+      },
+      options: {
+        enableMenu: true,
+        renderActions: {
+          findScript: [10, function (doc) {
+            document.querySelectorAll('span.math').forEach(function (node) {
+              var math = node.textContent;
+              node.textContent = math;
+            });
+          }, '']
+        }
+      },
+      loader: {load: ['[tex]/ams', '[tex]/noerrors', '[tex]/noundefined']}
+    };
+
     var script = document.createElement('script');
-    script.src = "/assets/js-managed/mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+    script.src = "/assets/js-managed/mathjax/es5/tex-mml-chtml.js";
     script.async = true;
     document.head.appendChild(script);
   }
