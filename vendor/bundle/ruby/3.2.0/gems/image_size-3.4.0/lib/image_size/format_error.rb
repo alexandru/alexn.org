dc6e7c9bedf96d8214ef7689caefc07a9bdd45f6
Alexandru Nedelcu <noreply@alexn.org>
2025-10-21 08:34:12 +0300

    Remove crap

--- vendor/bundle/ruby/3.2.0/gems/image_size-3.4.0/lib/image_size/format_error.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ImageSize
-  class FormatError < StandardError; end
-end
--- vendor/bundle/ruby/3.2.0/gems/sass-embedded-1.93.2-x86_64-linux-gnu/lib/sass/embedded/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sass
-  module Embedded
-    VERSION = '1.93.2'
-  end
-end
